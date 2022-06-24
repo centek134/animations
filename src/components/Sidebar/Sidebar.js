9e31086c885ca301922d0cc2bd4f6f0e0ce66aa9
centek134 <centek134@wp.pl>
2022-06-24 19:07:11 +0200

    Folder Folder that contains components

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../assets/scss/components/sidebar/sidebar.css";
+const Sidebar = () => {
+  return (
+    <nav className='side-nav'>
+        
+    </nav>
+  )
+}
+export default Sidebar;
