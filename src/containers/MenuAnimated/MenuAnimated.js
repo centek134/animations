a3ed9910ad319696008ecce2cca5cdc1d7a25126
centek134 <centek134@wp.pl>
2022-06-24 19:08:21 +0200

    First animation that's added to our project

---
+++ src/containers/MenuAnimated/MenuAnimated.js
@@ -0,0 +1,33 @@
+/* eslint-disable react/style-prop-object */
+import React from "react";
+
+const MenuAnimated = () => {
+    const url = "#";
+
+  return (
+    <section className="wrapper">
+      <ul>
+        <li style={{ "--i":6}}>
+          <a href={url}>Home</a>
+        </li>
+        <li style={{ "--i":5}}>
+          <a href={url}>about</a>
+        </li>
+        <li style={{ "--i":4}}>
+          <a href={url}>our team</a>
+        </li>
+        <li style={{ "--i":3}}>
+          <a href={url}>Products</a>
+        </li>
+        <li style={{ "--i":2}}>
+          <a href={url}>Portfolio</a>
+        </li>
+        <li style={{ "--i":1}}>
+          <a href={url}>Contact</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default MenuAnimated;
