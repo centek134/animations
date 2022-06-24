e0f3876520feccf707ce2de690e16f6ab190d0aa
centek134 <centek134@wp.pl>
2022-06-24 19:08:55 +0200

    Home Page container I'll think about it later, but I've added it for now, its going to be greeting for the person that visit our website

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../assets/scss/containers/menuanimated/menuanimated.css';
+import "../../assets/scss/containers/homepage/homepage.css";
+const HomePage = () => {
+  return (
+    <section className='home_page'>
+      <h1 id='home_page_h'><span>Welcome to home page!</span><span>Please explore animations that I've made</span></h1>
+    </section>
+  );
+};
+export default HomePage;
