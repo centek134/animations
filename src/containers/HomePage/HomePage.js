323aa715b89e34750d37884fefdf926528450bf4
centek134 <centek134@wp.pl>
2022-06-25 12:01:32 +0200

    changes in html structure

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import '../../assets/scss/containers/menuanimated/menuanimated.css';
+import React from "react";
+import "../../assets/scss/containers/menuanimated/menuanimated.css";
 import "../../assets/scss/containers/homepage/homepage.css";
 const HomePage = () => {
   return (
-    <section className='home_page'>
-      <h1 id='home_page_h'><span>Welcome to home page!</span><span>Please explore animations that I've made</span></h1>
+    <section className="home_page">
+      <h1>Welcome to home page!</h1>
+      <h2>Please explore animations that I've made.</h2>
     </section>
   );
 };
