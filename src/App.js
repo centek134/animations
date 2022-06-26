0fa1b836fda1180156dfb1b9376c2db164fa0214
centek134 <centek134@wp.pl>
2022-06-26 12:43:27 +0200

    First scratches of routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './assets/scss/containers/app/app.css';
+import {Routes, Route} from  'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar.js';
-import Homepage from './containers/HomePage/HomePage.js';
 import MenuAnimated from './containers/MenuAnimated/MenuAnimated';
+import HomePage from './containers/HomePage/HomePage.js';
 
 const App = () => {
   return (
     <div className="App">
-      <Sidebar/>
-      <Homepage/>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/menu-animated' element={<MenuAnimated/>}/>
+      </Routes>
     </div>
   );
 }
