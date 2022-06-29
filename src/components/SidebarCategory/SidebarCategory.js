f1960a8ec556b902e3e01695daec338dd4424296
centek134 <centek134@wp.pl>
2022-06-29 21:55:13 +0200

    changes added arrow that tells us if menu is opened or not added hideList state, depends of that our category renders a menu

--- src/components/SidebarCategory/SidebarCategory.js
+++ src/components/SidebarCategory/SidebarCategory.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {Link} from "react-router-dom";
 import "../../assets/scss/components/sidebarcategory/sidebarcategory.css";
 const SidebarCategory = (props) => {
+  const [hideList, setHideList] = useState(true);
   const list = props.list;
+  const textArrowDirection = () => {
+    const arrow = document.querySelector(".category_text_arrow");
+    if(!hideList){
+      arrow.classList.remove("show");
+    }
+    else{
+      arrow.classList.add("show");
+    };
+  };
+
   return (
       <div className="category">
-        <h3 className="category_text">{props.category_text}</h3>
-        <div className="list">
-            {list.map((item,i) => {
-              return <Link key={i} className='list_item' to={item.url}>{item.name}</Link>
-            })}
+        <h3 onClick={() => {setHideList(!hideList); textArrowDirection(); }} className="category_text">{props.category_text}<div className="category_text_arrow"></div></h3>
+        <div id="list" className="category_list">
+          {hideList? null
+          :
+          list.map((item,i) => {
+            return <Link key={i} className='list_item' to={item.url}>{item.name}</Link>
+          })}
         </div>
       </div>
   )
