a800b28219f21a220887b077d00f6dadffebe44f
centek134 <centek134@wp.pl>
2022-06-30 07:52:45 +0200

    changes found better way to switch classname of arrow, now its tied to hideList state

--- src/components/SidebarCategory/SidebarCategory.js
+++ src/components/SidebarCategory/SidebarCategory.js
@@ -4,19 +4,10 @@ import "../../assets/scss/components/sidebarcategory/sidebarcategory.css";
 const SidebarCategory = (props) => {
   const [hideList, setHideList] = useState(true);
   const list = props.list;
-  const textArrowDirection = () => {
-    const arrow = document.querySelector(".category_text_arrow");
-    if(!hideList){
-      arrow.classList.remove("show");
-    }
-    else{
-      arrow.classList.add("show");
-    };
-  };
 
   return (
       <div className="category">
-        <h3 onClick={() => {setHideList(!hideList); textArrowDirection(); }} className="category_text">{props.category_text}<div className="category_text_arrow"></div></h3>
+        <h3 onClick={() => {setHideList(!hideList);}} className="category_text">{props.category_text}<div className={hideList? "category_text_arrow" : "category_text_arrow show"}></div></h3>
         <div id="list" className="category_list">
           {hideList? null
           :
