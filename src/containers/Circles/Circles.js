b64db156b7e837aad9e2b96737898ff85ade4543
centek134 <centek134@wp.pl>
2022-07-08 18:22:08 +0200

    Changes new container with animation

---
+++ src/containers/Circles/Circles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../../assets/scss/containers/circles/circles.css";
+const Circles = () => {
+  return (
+    <section className="wrapper_circles">
+        <div className="scene">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+        </div>
+    </section>
+  );
+};
+
+export default Circles;
