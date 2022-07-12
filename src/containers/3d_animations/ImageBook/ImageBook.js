1ca3d9a6cb2d95b20bc328ae3cc53ad2f82dee5b
centek134 <centek134@wp.pl>
2022-07-12 07:02:33 +0200

    New functionality Implemented a way to grab background of wrapper and rotate in X-axis our book to see how it is looking in different angle

--- src/containers/3d_animations/ImageBook/ImageBook.js
+++ src/containers/3d_animations/ImageBook/ImageBook.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "../../../assets/scss/containers/3d_animations/ImageBook/ImageBook.css";
 export const ImageBook = () => {
+  let mouseClick = false;
+  let rotateDegree = 0;
+
+  const checkForClick = () => {
+    mouseClick = true;
+  }
+  const noClick = () => {
+    mouseClick = false;
+  }
+  const rotateX = (e) => {
+    const imageBookId = document.getElementById("book_3d");
+    if(mouseClick){
+      if(rotateDegree >= 70){
+        imageBookId.style.transform = `rotateX(0deg)`;
+      }
+      rotateDegree = e.pageY/20;
+      imageBookId.style.transform = `rotateX(${rotateDegree}deg)`;
+    }
+  };
   return (
-    <section className="wrapper_book">
+    <section onMouseDown={(e) => checkForClick(e)} onMouseUp={noClick} onMouseMove={(e) => rotateX(e)} id="wrapper_book" className="wrapper_book">
         <div id="book_3d" className="book">
           <input className="inpt" type="checkbox"/>
           <div className="page p_1"></div>
