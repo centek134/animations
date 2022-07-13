1bf45bf6021fb795f3b31ccc77c371a624480fe8
centek134 <centek134@wp.pl>
2022-07-13 18:39:15 +0200

    New animation It's not finished yet, but I'll be working on it

---
+++ src/containers/3d_animations/Sphere/Sphere.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../../assets/scss/containers/3d_animations/Sphere/Sphere.css";
+const Sphere = () => {
+  return (
+    <section className="wrapper_sphere">
+        <div className="sphere">
+            <div className="circle c_1"></div>
+            <div className="circle c_2"></div>
+            <div className="circle c_3"></div>
+            <div className="circle c_4"></div>
+            <div className="circle c_5"></div>
+            <div className="circle c_6"></div>
+            <div className="circle c_7"></div>
+            <div className="circle c_8"></div>
+            <div className="circle c_9"></div>
+            <div className="circle c_10"></div>
+        </div>
+    </section>
+  );
+};
+
+export default Sphere;
