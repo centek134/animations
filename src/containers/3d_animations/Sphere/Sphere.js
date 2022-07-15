9ef390503503226a7921f8bedca8cb41667f71e4
centek134 <centek134@wp.pl>
2022-07-15 19:22:09 +0200

    changes added few circles and roteted them in X axis

--- src/containers/3d_animations/Sphere/Sphere.js
+++ src/containers/3d_animations/Sphere/Sphere.js
@@ -4,16 +4,26 @@ const Sphere = () => {
   return (
     <section className="wrapper_sphere">
         <div className="sphere">
-            <div className="circle c_1"></div>
-            <div className="circle c_2"></div>
-            <div className="circle c_3"></div>
-            <div className="circle c_4"></div>
-            <div className="circle c_5"></div>
-            <div className="circle c_6"></div>
-            <div className="circle c_7"></div>
-            <div className="circle c_8"></div>
-            <div className="circle c_9"></div>
-            <div className="circle c_10"></div>
+            <div className="circle y_1"></div>
+            <div className="circle y_2"></div>
+            <div className="circle y_3"></div>
+            <div className="circle y_4"></div>
+            <div className="circle y_5"></div>
+            <div className="circle y_6"></div>
+            <div className="circle y_7"></div>
+            <div className="circle y_8"></div>
+            <div className="circle y_9"></div>
+            <div className="circle y_10"></div>
+            <div className="circle x_1"></div>
+            <div className="circle x_2"></div>
+            <div className="circle x_3"></div>
+            <div className="circle x_4"></div>
+            <div className="circle x_5"></div>
+            <div className="circle x_6"></div>
+            <div className="circle x_7"></div>
+            <div className="circle x_8"></div>
+            <div className="circle x_9"></div>
+            <div className="circle x_10"></div>
         </div>
     </section>
   );
