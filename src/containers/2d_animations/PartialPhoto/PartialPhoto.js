b1c5f747e577f10e19585250be9c7f7aa163e2bf
centek134 <centek134@wp.pl>
2022-07-16 17:05:33 +0200

    new animation this is 2d animations with photo. When you hover over inside of dashed space the photo will joing together

---
+++ src/containers/2d_animations/PartialPhoto/PartialPhoto.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../../assets/scss/containers/2d_animations/PartialPhoto/PartialPhoto.css";
+
+const PartialPhoto = () => {
+  return (
+    <div className="wrapper_partiialphoto">
+        <div className="scene">
+            <div className="img i_1"></div>
+            <div className="img i_2"></div>
+            <div className="img i_3"></div>
+            <div className="img i_4"></div>
+        </div>
+    </div>
+  );
+};
+
+export default PartialPhoto;
