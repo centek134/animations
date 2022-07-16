523a613c6e43dcb1f83392a37f3805255dc335e4
centek134 <centek134@wp.pl>
2022-07-16 17:06:48 +0200

    Added new route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import {React, useState} from "react";
 import "./assets/scss/containers/App/App.css";
 import {Routes, Route} from  "react-router-dom";
-import Sidebar from "./components/Sidebar/Sidebar.js";
-import MenuAnimated from "./containers/3d_animations/MenuAnimated/MenuAnimated";
 import HomePage from "./containers/HomePage/HomePage.js";
+import Sidebar from "./components/Sidebar/Sidebar.js";
+// =============== 3D animations imports ===============
+import MenuAnimated from "./containers/3d_animations/MenuAnimated/MenuAnimated";
 import Cube from "./containers/3d_animations/Cube/Cube";
 import Circles from "./containers/3d_animations/Circles/Circles";
 import ImageBook from "./containers/3d_animations/ImageBook/ImageBook";
 import Sphere from "./containers/3d_animations/Sphere/Sphere";
+//=====================================================
+
+//=============== 2D animations imports ===============
+import PartialPhoto from "./containers/2d_animations/PartialPhoto/PartialPhoto";
 
 const App = () => {
 const [showSidebar, setShowSidebar] = useState(false);
@@ -26,6 +31,7 @@ const [showSidebar, setShowSidebar] = useState(false);
         <Route path="/3d-animations/circles" element={<Circles/>}/>
         <Route path="/3d-animations/book" element={<ImageBook/>}/>
         <Route path="/3d-animations/sphere" element={<Sphere/>}/>
+        <Route path="/2d-animations/partial-photo" element={<PartialPhoto/>}/>
       </Routes>
       </div>
     </div>
