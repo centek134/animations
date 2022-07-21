a2e25edd12c0674e26ab6d5415726717f6acf55b
centek134 <centek134@wp.pl>
2022-07-21 18:51:01 +0200

    New container with animation

---
+++ src/containers/3d_animations/Text/Text.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../../../assets/scss/containers/3d_animations/Text/Text.css";
+export const Text = () => {
+  return (
+    <section className="wrapper_text">
+      <div className="scene">
+        <div className="box">
+          <span style={{ "--i":0}} className="item"></span>
+          <span style={{ "--i":1}} className="item"></span>
+          <span style={{ "--i":2}} className="item"></span>
+          <span style={{ "--i":3}} className="item"></span>
+          <span style={{ "--i":4}} className="item"></span>
+          <span style={{ "--i":5}} className="item"></span>
+          <span style={{ "--i":6}} className="item"></span>
+          <span style={{ "--i":7}} className="item"></span>
+          <span style={{ "--i":8}} className="item"></span>
+          <span style={{ "--i":9}} className="item"></span>
+          <span style={{ "--i":10}} className="item"></span>
+          <span style={{ "--i":11}} className="item"></span>
+          <span style={{ "--i":12}} className="item"></span>
+          <span style={{ "--i":13}} className="item"></span>
+          <span style={{ "--i":14}} className="item"></span>
+          <span style={{ "--i":15}} className="item"></span>
+          <span style={{ "--i":16}} className="item"></span>
+          <span style={{ "--i":17}} className="item"></span>
+          <span style={{ "--i":18}} className="item"></span>
+          <span style={{ "--i":19}} className="item"></span>
+          <span style={{ "--i":20}} className="item"></span>
+          <span style={{ "--i":21}} className="item"></span>
+          <span style={{ "--i":22}} className="item"></span>
+          <span style={{ "--i":23}} className="item"></span>
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Text;
