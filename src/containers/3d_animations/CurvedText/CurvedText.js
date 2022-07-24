32c518734838c6adb7121dc5c2c66764857b75cf
centek134 <centek134@wp.pl>
2022-07-24 12:31:31 +0200

    changes I approached this animation from a different angle, by using sudo elements (:before).

---
+++ src/containers/3d_animations/CurvedText/CurvedText.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../../../assets/scss/containers/3d_animations/CurvedText/CurvedText.css";
+export const CurvedText = () => {
+  return (
+    <section className="wrapper_curved_text">
+      <div className="scene">
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
+    </section>
+  );
+};
+export default CurvedText;
