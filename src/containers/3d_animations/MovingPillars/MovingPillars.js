fc09ce41ddcf55c47fc5bc707b6855433162f927
centek134 <centek134@wp.pl>
2022-07-28 07:00:39 +0200

    new animation added moving pillars animation

---
+++ src/containers/3d_animations/MovingPillars/MovingPillars.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../../assets/scss/containers/3d_animations/MovingPillars/MovingPillars.css";
+
+const MovingPillars = () => {
+  return (
+    <section className="wrapper_moving_pillars">
+      <ul className="list">
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+      </ul>
+    </section>
+  );
+};
+export default MovingPillars;
