6609a5f2953f2f78fa5335a9757b8bf75aa68526
centek134 <centek134@wp.pl>
2022-07-31 10:00:22 +0200

    A lot of changes
    So in order to keep code clean I've used barrel exports. Beside that I've reorganized Routes for both animations, and changed approach from static to dynamic.
    Now our routes are stored in objects that are stored in array (each animation category have its own array), and I'm mapping through that array returing routes. It's really saving a lot of space, also thats super easy solution so it's easy to maintenance.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import {React, useState} from "react";
-import "./assets/scss/containers/App/App.css";
 import {Routes, Route} from  "react-router-dom";
 import HomePage from "./containers/HomePage/HomePage.js";
 import Sidebar from "./components/Sidebar/Sidebar.js";
+import "./assets/scss/containers/App/App.css";
 // =============== 3D animations imports ===============
-import MenuAnimated from "./containers/3d_animations/MenuAnimated/MenuAnimated";
-import Cube from "./containers/3d_animations/Cube/Cube";
-import Circles from "./containers/3d_animations/Circles/Circles";
-import ImageBook from "./containers/3d_animations/ImageBook/ImageBook";
-import Sphere from "./containers/3d_animations/Sphere/Sphere";
-import Text from "./containers/3d_animations/Text/Text";
-import CurvedText from "./containers/3d_animations/CurvedText/CurvedText";
-import MovingPillars from "./containers/3d_animations/MovingPillars/MovingPillars";
-//=====================================================
+import {MenuAnimated, Cube, Circles, ImageBook, Sphere, Text, CurvedText, MovingPillars} from "./containers/3d_animations/index";
+//=============== 2D animations imports ================
+import {HideAndSeek, Dots} from "./containers/2d_animations/index";
 
-//=============== 2D animations imports ===============
-import Dots from "./containers/2d_animations/Dots/Dots";
-import HideAndSeek from "./containers/2d_animations/HideAndSeek/HideAndSeek";
-
-//=====================================================
 const App = () => {
 const [showSidebar, setShowSidebar] = useState(false);
+
+const animations_route_3d = [
+{path:"/3d-animations/menu-animated", element:<MenuAnimated/>},{path:"/3d-animations/cube", element:<Cube/>},{path:"/3d-animations/circles", element:<Circles/>}, 
+{path:"/3d-animations/book", element:<ImageBook/>}, {path:"/3d-animations/sphere", element:<Sphere/>}, {path:"/3d-animations/text", element:<Text/>}, {path:"/3d-animations/curved-text", element:<CurvedText/>}, {path:"/3d-animations/moving-pillars", element:<MovingPillars/>}
+];
+const animations_route_2d = [{path:"/2d-animations/hide-and-seek", element:<HideAndSeek/>},{path:"/2d-animations/dots", element:<Dots/>}];
 
   return (
     <div className="App">
@@ -32,22 +27,13 @@ const [showSidebar, setShowSidebar] = useState(false);
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         {/*3d animations routes */}
-        <Route path="/3d-animations/menu-animated" element={<MenuAnimated/>}/>
-        <Route path="/3d-animations/cube" element={<Cube/>}/>
-        <Route path="/3d-animations/circles" element={<Circles/>}/>
-        <Route path="/3d-animations/book" element={<ImageBook/>}/>
-        <Route path="/3d-animations/sphere" element={<Sphere/>}/>
-        <Route path="/3d-animations/text" element={<Text/>}/>
-        <Route path="/3d-animations/curved-text" element={<CurvedText/>}/>
-        <Route path="/3d-animations/moving-pillars" element={<MovingPillars/>}/>
-
+        {animations_route_3d.map(item => <Route path={item.path} element = {item.element}/>)}
         {/*2d animations routes */}
-        <Route path="/2d-animations/hide-and-seek" element={<HideAndSeek/>}/>
-        <Route path="/2d-animations/dots" element={<Dots/>}/>
+        {animations_route_2d.map(item => <Route path={item.path} element = {item.element}/>)}
       </Routes>
       </div>
     </div>
   );
-}
+};
 
 export default App;
