e0d9894405e5f24abd071a45de00b25704427c11
centek134 <centek134@wp.pl>
2022-08-02 07:42:37 +0200

    hexagon madness file

---
+++ src/containers/2d_animations/HexagonMadness/HexagonMadness.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "../../../assets/scss/containers/2d_animations/HexagonMadness/HexagonMadness.css"
+
+export const HexagonMadness = () => {
+  return (
+    <section class="wrapper_hexamania">
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+    </section>
+  );
+};
