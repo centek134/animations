578ceb7476545ccd9cbbf4e20f5d8fc2e0ec8fab
centek134 <centek134@wp.pl>
2022-08-07 12:06:03 +0200

    new animation added new animation "hexagons" as always I've imported it from index.js barrel file and added to 2danimations route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./assets/scss/containers/App/App.css";
 // =============== 3D animations imports ===============
 import {MenuAnimated, Cube, Circles, ImageBook, Sphere, Text, CurvedText, MovingPillars, Transformer} from "./containers/3d_animations/index";
 //=============== 2D animations imports ================
-import {HideAndSeek, Dots, HexagonMadness} from "./containers/2d_animations/index";
+import {HideAndSeek, Dots, HexagonMadness, Hexagons} from "./containers/2d_animations/index";
 
 const App = () => {
 const [showSidebar, setShowSidebar] = useState(false);
@@ -15,7 +15,7 @@ const animations_route_3d = [
 {path:"/3d-animations/menu-animated", element:<MenuAnimated/>},{path:"/3d-animations/cube", element:<Cube/>},{path:"/3d-animations/circles", element:<Circles/>}, 
 {path:"/3d-animations/book", element:<ImageBook/>}, {path:"/3d-animations/sphere", element:<Sphere/>}, {path:"/3d-animations/text", element:<Text/>}, {path:"/3d-animations/curved-text", element:<CurvedText/>}, {path:"/3d-animations/moving-pillars", element:<MovingPillars/>},
 {path:"/3d-animations/transformer", element:<Transformer/>}];
-const animations_route_2d = [{path:"/2d-animations/hide-and-seek", element:<HideAndSeek/>},{path:"/2d-animations/dots", element:<Dots/>},{path:"/2d-animations/hexagon-madness", element:<HexagonMadness/>}];
+const animations_route_2d = [{path:"/2d-animations/hide-and-seek", element:<HideAndSeek/>},{path:"/2d-animations/dots", element:<Dots/>},{path:"/2d-animations/hexagons", element:<Hexagons/>},{path:"/2d-animations/hexagon-madness", element:<HexagonMadness/>}];
 
   return (
     <div className="App">
