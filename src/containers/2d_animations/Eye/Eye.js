6f28d93b7a7ddbb829220cefc4fd4703ce4d5ada
centek134 <centek134@wp.pl>
2022-08-09 13:20:52 +0200

    js file for eye animation

---
+++ src/containers/2d_animations/Eye/Eye.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../../assets/scss/containers/2d_animations/Eye/Eye.css";
+export const Eye = () => {
+    
+  return (
+    <section className="wrapper_eye">
+        <div className="eye">
+            <div className="wrapper_lens">
+                <div id="lens" className="lens"></div>
+            </div>
+        </div>
+    </section>
+  );
+};
