42d84b5fab69c0f52940d395a6ea1ec8d7a2b1a6
centek134 <centek134@wp.pl>
2022-08-09 13:19:57 +0200

    added eye container to barrel export

--- src/containers/2d_animations/index.js
+++ src/containers/2d_animations/index.js
@@ -2,3 +2,4 @@ export * from "./Dots/Dots.js";
 export * from "./HideAndSeek/HideAndSeek.js";
 export * from "./HexagonMadness/HexagonMadness.js";
 export * from "./Hexagons/Hexagons.js";
+export * from "./Eye/Eye.js";
