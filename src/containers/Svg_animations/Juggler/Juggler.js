1105551d1b80367c42bfca1f98798e3e65cf0b71
centek134 <centek134@wp.pl>
2022-08-15 19:44:57 +0200

    juggler js file

--- src/containers/Svg_animations/Juggler/Juggler.js
+++ src/containers/Svg_animations/Juggler/Juggler.js
@@ -3,7 +3,9 @@ import "../../../assets/scss/containers/Svg_animations/Juggler/Juggler.css";
 import {ReactComponent as Man} from "../../../assets/svg/Juggler/man_550w_550h.svg";
 import {ReactComponent as LeftHand} from "../../../assets/svg/Juggler/left_hand_pc.svg";
 import {ReactComponent as RightHand} from "../../../assets/svg/Juggler/right_hand_pc.svg";
-import {ReactComponent as Ball} from "../../../assets/svg/Juggler/ball.svg";
+import {ReactComponent as GreenBall} from "../../../assets/svg/Juggler/green_ball.svg";
+import {ReactComponent as YellowBall} from "../../../assets/svg/Juggler/yellow_ball.svg";
+import {ReactComponent as BlueBall} from "../../../assets/svg/Juggler/blue_ball.svg";
 
 export const Juggler = () => {
   return (
@@ -12,9 +14,9 @@ export const Juggler = () => {
             <Man/>
             <LeftHand className = "left_hand"/>
             <RightHand className = "right_hand"/>
-            <Ball className = "ball nr_1"/>
-            <Ball className = "ball nr_2"/>
-            <Ball className = "ball nr_3"/>
+            <GreenBall className = "ball green"/>
+            <YellowBall className = "ball yellow"/>
+            <BlueBall className = "ball blue"/>
         </div>
         
     </section>
