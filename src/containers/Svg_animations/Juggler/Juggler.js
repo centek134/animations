449278f6257ecb18ec7f77aae3ed38d5de53212b
centek134 <centek134@wp.pl>
2022-08-17 15:56:22 +0200

    imported mobile svg as components

--- src/containers/Svg_animations/Juggler/Juggler.js
+++ src/containers/Svg_animations/Juggler/Juggler.js
@@ -7,6 +7,13 @@ import {ReactComponent as GreenBall} from "../../../assets/svg/Juggler/green_bal
 import {ReactComponent as YellowBall} from "../../../assets/svg/Juggler/yellow_ball.svg";
 import {ReactComponent as BlueBall} from "../../../assets/svg/Juggler/blue_ball.svg";
 
+import {ReactComponent as ManMobile} from "../../../assets/svg/Juggler/man_300w_300h.svg";
+import {ReactComponent as LeftHandMobile} from "../../../assets/svg/Juggler/left_hand_mobile.svg";
+import {ReactComponent as RightHandMobile} from "../../../assets/svg/Juggler/right_hand_mobile.svg";
+import {ReactComponent as GreenBallMobile} from "../../../assets/svg/Juggler/green_ball_mobile.svg";
+import {ReactComponent as YellowBallMobile} from "../../../assets/svg/Juggler/yellow_ball_mobile.svg";
+import {ReactComponent as BlueBallMobile} from "../../../assets/svg/Juggler/blue_ball_mobile.svg";
+
 export const Juggler = () => {
   return (
     <section className="wrapper_juggler">
@@ -17,6 +24,14 @@ export const Juggler = () => {
             <GreenBall className = "ball green"/>
             <YellowBall className = "ball yellow"/>
             <BlueBall className = "ball blue"/>
+        </div>
+        <div className="man_container_mobile">
+            <ManMobile/>
+            <LeftHandMobile className = "left_hand_mobile"/>
+            <RightHandMobile className = "right_hand_mobile"/>
+            <GreenBallMobile className = "ball green_mobile"/>
+            <YellowBallMobile className = "ball yellow_mobile"/>
+            <BlueBallMobile className = "ball blue_mobile"/>
         </div>
         
     </section>
