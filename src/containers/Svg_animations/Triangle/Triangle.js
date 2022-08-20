3f082cc8b948cb0fd9f24102c84094033055c086
centek134 <centek134@wp.pl>
2022-08-20 13:04:09 +0200

    added triangle container

---
+++ src/containers/Svg_animations/Triangle/Triangle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactComponent as Item} from "../../../assets/svg/Triangle/Triangle.svg";
+
+export const Triangle = () => {
+  return (
+    <section className="wrapper_triangle">
+        
+    </section>
+  );
+};
