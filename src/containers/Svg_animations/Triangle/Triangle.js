278db9170fe5d51934624ddf4b2e2844be330a21
centek134 <centek134@wp.pl>
2022-08-21 12:46:21 +0200

    imported couple svg

--- src/containers/Svg_animations/Triangle/Triangle.js
+++ src/containers/Svg_animations/Triangle/Triangle.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "../../../assets/scss/containers/Svg_animations/Triangle/Triangle.css";
 import { ReactComponent as Item} from "../../../assets/svg/Triangle/Triangle.svg";
 
 export const Triangle = () => {
   return (
     <section className="wrapper_triangle">
-        
+        <div className="scene">
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+          <Item className="triangle"/>
+        </div>
     </section>
   );
 };
